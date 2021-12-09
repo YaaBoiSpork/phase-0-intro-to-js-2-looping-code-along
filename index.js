3dd4af7655a9a3dd83d91a067396aa80324f41f6
Stephen Nerby <stephen.a.nerby@gmail.com>
2021-12-08 21:36:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const name = [`Guadalupe`, `Ollie`, `Aki`]
+
+function writeCards(name) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+
+    return messages;
+} 
+
+writeCards(name);
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
